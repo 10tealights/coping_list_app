48c6198ae67b7716c393ac111f4079571647afd8
戸塚翔子 <99260932+10tealights@users.noreply.github.com>
2022-05-06 14:24:52 +0900

    Add: user_sessionsのnew,create,destroyアクション追加・viewファイルのform_with作成

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,4 +1,27 @@
 class UserSessionsController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = login(params[:email], params[:password])
+
+    if @user
+      redirect_back_or_to(root_url, notice: 'Login successful')
+    else
+      flash[:alert] = 'Login failed'
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to(:users, notice: 'Logged out!')
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password)
   end
 end
