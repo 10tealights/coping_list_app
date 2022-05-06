190d81c6dac41f094df0be465578e7b02fc0af72
戸塚翔子 <99260932+10tealights@users.noreply.github.com>
2022-05-06 20:59:48 +0900

    Add: coping_listsのindex,new,createアクション追加・対応するviewファイル内容作成

--- app/controllers/coping_lists_controller.rb
+++ app/controllers/coping_lists_controller.rb
@@ -1,13 +1,31 @@
 class CopingListsController < ApplicationController
   def index
+    @coping_lists = current_user.coping_lists
   end
   
   def new
+    @coping_list = CopingList.new
+  end
+
+  def create
+    @coping_list = current_user.coping_lists.build(coping_list_params)
+    if @coping_list.save
+      redirect_to coping_lists_url, notice: 'Coping lists was successfully created'
+    else
+      flash[:alert] = 'Creating coping lists failed'
+      render :new
+    end
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def coping_list_params
+    params.require(:coping_list).permit(:list_name)
   end
 end
