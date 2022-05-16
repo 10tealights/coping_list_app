c2959435a833c0d8b1769892301a7cd3ef9caf38
戸塚翔子 <99260932+10tealights@users.noreply.github.com>
2022-05-16 16:20:45 +0900

    Fix: Historyモデルに気分変化量と評価のバリデーションを追加

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,3 +1,6 @@
 class History < ApplicationRecord
   belongs_to :coping
+
+  validates :change_amount, presence: true, numericality: { greater_than_or_equal_to: -100, less_than_or_equal_to: 100 }
+  validates :evaluation, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
