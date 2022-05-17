580668f672ffd97d347d0c6a3cafbcf2f341e5c3
戸塚翔子 <99260932+10tealights@users.noreply.github.com>
2022-05-17 12:19:04 +0900

    Add: rails-i18nのbundle installおよびapplication.rbにてlocale・TimeZone設定

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module CopingListApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     # Configuration for the application, engines, and railties goes here.
     config.generators do |g|
@@ -34,8 +36,8 @@ module CopingListApp
 
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
