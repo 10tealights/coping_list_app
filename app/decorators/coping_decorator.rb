35386d14e936618c2ed1e1a22dde4df5228077ea
戸塚翔子 <99260932+10tealights@users.noreply.github.com>
2022-05-18 13:23:20 +0900

    Fix: 費用の金額データが空だった場合のNoMethodErrorを修正

--- app/decorators/coping_decorator.rb
+++ app/decorators/coping_decorator.rb
@@ -3,6 +3,6 @@ class CopingDecorator < Draper::Decorator
 
   # 金額をカンマありの表示にする
   def cost_with_comma
-    cost_amount.to_s(:delimited)
+    cost_amount.to_s(:delimited) if cost_amount.present?
   end
 end
