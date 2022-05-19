6dc2748e67098a1d5e1e8092b62c999559e59b77
戸塚翔子 <99260932+10tealights@users.noreply.github.com>
2022-05-19 22:51:23 +0900

    Add: OGP画像と設定の追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,34 @@ module ApplicationHelper
     base_title = "Copin'"
     page_title.empty? ? base_title : page_title + ' | ' + base_title
   end
+  
+  # 各ページの説明文　120文字前後
+  def full_description(page_description = '')
+    base_description = "Copin'(コーピン)はコーピングリストを簡単に、複数作成できるサービスです。コーピング(coping)とは「ストレス対処」のこと。ストレス対策や気分転換、気晴らしの方法をリスト化して、いつでも見られるようにしませんか？"
+    if page_description.empty?
+      base_description
+    else
+      page_description
+    end
+  end
+  
+  # 各ページの説明文 50文字前後
+  def og_description(page_description = '')
+    base_description = "Copin'はコーピング(ストレス対処)リストを簡単に複数作成できるサービスです。"
+    if page_description.empty?
+      base_description
+    else
+      page_description
+    end
+  end
+  
+  # 各ページのイメージ画像
+  def og_image(page_image = '')
+    base_image = image_url('copin_ogp.jpg')
+    if page_image.empty?
+      base_image
+    else
+      page_image
+    end
+  end
 end
