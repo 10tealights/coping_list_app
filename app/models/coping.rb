8da60cbb3be772fe2bbd2c742afc9c774823f5b5
戸塚翔子 <99260932+10tealights@users.noreply.github.com>
2022-05-19 13:35:44 +0900

    Add,Improve: 絵文字のバリデーション追加およびコーピング項目抽出処理のscope化

--- app/models/coping.rb
+++ app/models/coping.rb
@@ -3,8 +3,11 @@ class Coping < ApplicationRecord
   has_many :histories, dependent: :destroy
 
   validates :coping_name, presence: true
-  validates :emoji, presence: true
+  validates :emoji, presence: true, format: { with: /[\p{Emoji}\p{Emoji_Component}&&[:^ascii:]]/, message: "のみ使えます(※一部使用できないものがあります)" }, length: { maximum: 3 }
   validates :status, presence: true
 
   enum status: { open: 0, close: 1 }
+
+  scope :high_avarage_rate, ->(coping_list) { joins(:histories).where(coping_list_id: coping_list.id).group(:id).limit(5).order('average_evaluation desc').average(:evaluation) }
+  scope :no_histories, -> { left_joins(:histories).where(histories: { id: nil }).sample(5) }
 end
