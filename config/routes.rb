22d36dcba68554cea49410652bdaa99ba0b562f4
戸塚翔子 <99260932+10tealights@users.noreply.github.com>
2022-05-24 00:01:51 +0900

    Add: 使い方を説明する静的ページ(guide)の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'static_pages#top'
+  get '/guide', to: 'static_pages#guide'
   get '/login', to: 'user_sessions#new'
   post '/login', to: 'user_sessions#create'
   delete '/logout', to: 'user_sessions#destroy'
